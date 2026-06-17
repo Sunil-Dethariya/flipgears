fb7e587ad39cb2910fdfe9968b2c7e46f56619c4
Sunil Dethariya <sunilahir9830@gmail.com>
2026-06-17 18:34:22 +0530

    Fix static files

--- pro/settings.py
+++ pro/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,3 +136,4 @@ RAZORPAY_KEY_ID = 'rzp_test_Sdce85lyLmLVPB'
 RAZORPAY_SECRET_KEY = 'kPCm0Oy64DqY70X6lQd5CCD2'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
